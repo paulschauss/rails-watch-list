20a2110df349648226e4d8a38d5730bd5469c0e5
Paul Schauss <p+github@schauss.fr>
2022-02-18 11:44:51 +0100

    end

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :lists, only: %i[index show create new] do
     resources :bookmarks, only: %i[create new]
   end
-  resources :bookmarks, only: [ :destroy ]
+  resources :bookmarks, only: [:destroy]
 end
